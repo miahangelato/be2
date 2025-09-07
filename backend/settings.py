d1cc57b895763f68662a74103b90882db49bcb9b
miah4 <miah@simpleprojex.com>
2025-09-07 18:05:36 +0800

    update cors

--- backend/settings.py
+++ backend/settings.py
@@ -182,7 +182,8 @@ else:
     CORS_ALLOWED_ORIGINS = [
         "http://localhost:3000",  # Local development
         "http://127.0.0.1:3000",  # Local development
-        "https://fe-zeta-bay.vercel.app/",  # Vercel deployment (update this)
+        "https://fe-zeta-bay.vercel.app/", 
+        "https://fe-hd7w.vercel.app/",  # Vercel deployment (update this)
     ]
     CORS_ALLOW_ALL_ORIGINS = False
 
